86dc9d9e57400b439a6897daa953808883d6bcb4
nobu-tech64 <douji533rikou@icloud.com>
2019-12-27 13:55:29 +0900

    messageテーブルの追加とその設定

--- db/schema.rb
+++ db/schema.rb
@@ -29,9 +29,14 @@ ActiveRecord::Schema.define(version: 20191226140406) do
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "text",       limit: 65535, null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "content"
+    t.string   "image"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
