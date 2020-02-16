c31d09a8c5954f8ee2f3dad590c5d1ed1cae1889
nobu-tech64 <douji533rikou@icloud.com>
2020-02-16 22:55:29 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,36 +1,28 @@
 $(function(){
 
-  function buildHTML(message){
+  var buildHTML = function(message) {
 
-    if (message.image){
-      var html = `<div class="chat-main__message-list__commenter">
-                    <div class="chat-main__message-list__commenter__name">
-                      ${message.name}
-                    </div>
-                    <div class="chat-main__message-list__commenter__time">
-                      ${message.created_at}
-                    </div>
+    if (message.content && message.image) {
+      var html = `<div class="chat-main__message-list__commenter" data-message-id=${message.id}>
+                    <div class="chat-main__message-list__commenter__name">${message.user_name}</div>
+                    <div class="chat-main__message-list__commenter__time">${message.created_at}</div>
                   </div>
-                  <div class="chat-main__message-list__content">
-                    ${message.content}
+                  <div class="chat-main__message-list__content">${message.content}</div>
+                  <img src="${message.image}" class="chat-main__message-list__image">`
+    } else if (message.content) {
+      var html = `<div class="chat-main__message-list__commenter" data-message-id=${message.id}>
+                    <div class="chat-main__message-list__commenter__name">${message.user_name}</div>
+                    <div class="chat-main__message-list__commenter__time">${message.created_at}</div>
                   </div>
-                  <img src=${message.image}>`
-      return html;
-    } else {
-      var html = `<div class="chat-main__message-list__commenter">
-                    <div class="chat-main__message-list__commenter__name">
-                      ${message.name}
-                    </div>
-                    <div class="chat-main__message-list__commenter__time">
-                      ${message.created_at}
-                    </div>
+                  <div class="chat-main__message-list__content">${message.content}</div>`
+    } else if (message.image) {
+      var html = `<div class="chat-main__message-list__commenter" data-message-id=${message.id}>
+                    <div class="chat-main__message-list__commenter__name">${message.user_name}</div>
+                    <div class="chat-main__message-list__commenter__time">${message.created_at}</div>
                   </div>
-                  <div class="chat-main__message-list__content">
-                    ${message.content}
-                  </div>`
-      return html;
-    }
-
+                  <img src="${message.image}" class="chat-main__message-list__image">`
+    };
+    return html;
   }
 
   $('#new_message').on('submit', function(e){
@@ -59,4 +51,31 @@ $(function(){
       $('.chat-main__message-form__new_message__sendbtn').prop('disabled', false);
     });
   });
+
+  var reloadMessages = function() {
+    last_message_id = $('.chat-main__message-list__commenter:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message){
+          insertHTML += buildHTML(message)
+        });
+        $(".chat-main__message-list").append(insertHTML);
+        $(".chat-main__message-list").animate({scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+  
 });
