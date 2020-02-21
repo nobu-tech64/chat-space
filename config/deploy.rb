0318b96504846d7135682bd0c6410823e2c429ff
nobu-tech64 <douji533rikou@icloud.com>
2020-02-21 21:00:10 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.12.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'chatspace'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:nobu-tech64/chat-space.git'
